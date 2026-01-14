10780f0bd67c5c77f66fd2818bd73aa17ac0e052
phamhieu <phamhieu171282@gmail.com>
2026-01-14 12:52:02 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from "express"
+
+
+const app = express()
+
+const hostname = "localhost"
+const PORT = process.env.PORT || 8000
+
+// middlewares
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("<h1></h1>Hello world</h1>")
+})
+
+connectDB()
+app.listen(PORT,hostname, (req, res)=>{
+    console.log(`Server run port http://${hostname}:${PORT}/`)
+
+})
+
+
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import { connectDB } from "./libs/db.js";
+// import authRoute from "./routes/authRoute.js";
+// import userRoute from "./routes/userRoute.js";
+// import cookieParser from "cookie-parser";
+// import { protectedRoute } from "./middlewares/authMiddleware.js";
+// import cors from "cors";
+
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5001;
+
+// // middlewares
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// // public routes
+// app.use("/api/auth", authRoute);
+
+// // private routes
+// app.use(protectedRoute);
+// app.use("/api/users", userRoute);
+
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`server bắt đầu trên cổng ${PORT}`);
+//   });
+// });
